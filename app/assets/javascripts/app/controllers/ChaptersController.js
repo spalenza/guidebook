845ecd003e65c1f3af42e2f45411026c1fc81228
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2014-01-09 13:44:52 -0200

    initalize with angular structure

--- app/assets/javascripts/app/controllers/ChaptersController.js
+++ app/assets/javascripts/app/controllers/ChaptersController.js
@@ -1,32 +1,5 @@
-// // Guidebook.controller('ChaptersController', function($scope, $location, $routeParams, ChapterModel, NoteModel) {
-// //   debugger;
-// //   // var chapters = ChapterModel.getChapters();
-// //   // for (var i = 0; i < chapters.length; i++) {
-// //   //   chapter[i].notes = NoteModel.getNotesForChapter(chapter[i].id);
-// //   // }
-// //   // $scope.chapters = chapters;
-// //   // $scope.selectedChapterId = $routesParams.chapterId;
-// //   // $scope.onDelete = function(noteId) {
-// //   //   var confirmDelete = confirm('Are you sure you want to delete this note?');
-// //   //   if (confirmDelete) {
-// //   //     $location.path('/deleteNote/' + $routeParams.chapterId + '/' + noteId);
-// //   //   }
-// //   // }
-// // });
-//
-//
-// phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
-//   function ($scope, $http) {
-//     debugger;
-//     $http.get('phones/phones.json').success(function(data) {
-//       $scope.phones = data;
-//     });
-//
-//     $scope.orderProp = 'age';
-//   }]);
-//
-// phonecatApp.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-//   function($scope, $routeParams) {
-//     debugger;
-//     $scope.phoneId = $routeParams.phoneId;
-//   }]);
+Guidebook.controller('ChaptersController', ['$scope',
+  function ($scope, $http) {
+    $scope.greeting = 'Hola!';
+  }
+]);
--- app/assets/javascripts/app/routes.js
+++ app/assets/javascripts/app/routes.js
@@ -1,19 +1,10 @@
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-]);
-
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
+Guidebook.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
+      when('/', {
+        templateUrl: '../assets/app/views/chapters.html',
+        controller: 'ChaptersController'
       });
-  }]);
+    $locationProvider.html5Mode(false);
+  }
+]);
