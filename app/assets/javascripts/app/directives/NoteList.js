8e57ab3976b5019bd8ec36f02416bec358336447
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2014-01-21 22:44:01 -0200

    final

--- app/assets/javascripts/app/directives/NoteList.js
+++ app/assets/javascripts/app/directives/NoteList.js
@@ -1,7 +1,13 @@
 Guidebook.directive('gbNoteList', function() {
   return {
     restrict: 'E',
-    templateUrl: '/assets/app/views/directives/noteList.html'
+    templateUrl: '/assets/app/views/directives/noteList.html',
+    scope: {
+      show: '=',
+      notes: '=',
+      orderValue: '@orderBy',
+      onDelete: '=deleteNoteHandler'
+    }
   };
 });
 
